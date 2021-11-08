c66682b477fa48d6d89af22d17895afb649c8295
cibelegoudinho <cibelegoudinho@frwk.com.br>
2021-11-08 19:52:43 -0300

    Added views to create/edit blog post inside the site

--- app/urls.py
+++ app/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.note_list, name='note_list'),
-    path('note/<int:pk>/', views.note_detail, name='note_detail')
+    path('note/<int:pk>/', views.note_detail, name='note_detail'),
+    path('note/new/', views.note_new, name='note_new'),
+    path('note/<int:pk>/edit/', views.note_edit, name='note_edit'),
 ]
--- app/views.py
+++ app/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from .models import Note
+from .forms import NoteForm
 
 def note_list(request):
     notes = Note.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
@@ -10,3 +11,30 @@ def note_list(request):
 def note_detail (request, pk):
     note = get_object_or_404(Note, pk=pk)
     return render(request, 'app/note_detail.html', {'note': note})
+
+def note_new(request):
+    if request.method == "POST":
+         form = NoteForm(request.POST)
+         if form.is_valid():
+             note = form.save(commit=False)
+             note.user = request.user
+             note.published_date = timezone.now()
+             note.save()
+             return redirect('note_detail', pk=note.pk)
+    else:
+        form = NoteForm()
+    return render(request, 'app/note_edit.html', {'form': form})
+
+def note_edit(request, pk):
+     note = get_object_or_404(Note, pk=pk)
+     if request.method == "POST":
+         form = NoteForm(request.POST, instance=note)
+         if form.is_valid():
+             note = form.save(commit=False)
+             note.user = request.user
+             note.published_date = timezone.now()
+             note.save()
+             return redirect('note_detail', pk=note.pk)
+     else:
+         form = NoteForm(instance=note)
+     return render(request, 'app/note_edit.html', {'form': form})
